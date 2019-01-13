185649f9c775acd56252f31668d67ab09e1b783b
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-13 22:15:58 +0900

    Open Express server of A-tube. Congrats

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const handleListening = port => {
+  console.log(`A-tube app listening on port ${port}`);
+};
+
+app.listen(PORT, handleListening(PORT));
