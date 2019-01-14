1b0fd206d975741ef7b6d537a88952711aba7650
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-14 15:34:58 +0900

    Understanding of middleware

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+const middlewareTestFN = (req, res, next) => {
+  console.log("middleware");
+  next();
+};
+app.use(middlewareTestFN);
+
 const handleListening = port => {
   console.log(`A-tube app listening on port ${port}`);
 };
+
+const handleHomeRoute = (req, res) => {
+  console.log("Home");
+  res.send("Home");
+};
+app.get("/", handleHomeRoute);
+const handleTestRoute = (req, res) => {
+  console.log("Test");
+  res.send("Test");
+};
+app.get("/test", handleTestRoute);
 
 app.listen(PORT, handleListening(PORT));
