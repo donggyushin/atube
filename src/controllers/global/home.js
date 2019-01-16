2135497f13a45c3432195801ee121725fcb40406
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-16 19:31:33 +0900

    Get some sample data from youtube

--- src/controllers/global/home.js
+++ src/controllers/global/home.js
@@ -1,6 +1,9 @@
+import { videos } from "../../db";
+
 const home = (req, res) => {
   const local = {
-    pageTitle: "HOME"
+    pageTitle: "HOME",
+    videos
   };
 
   res.render("routes/Home/home.ejs", local);
---
+++ src/db.js
@@ -0,0 +1,41 @@
+export const videos = [
+  {
+    id: 123123,
+    title: "Camila Cabello - Never Be the Same (Official Music Video)",
+    description:
+      "(C) 2018 Simco Ltd. under exclusive license to Epic Records, a division of Sony Music ",
+    views: 177275745,
+    videoFile: "https://www.youtube.com/embed/Ph54wQG8ynk?list=RDPh54wQG8ynk",
+    creator: {
+      id: 123824,
+      name: "신동규",
+      email: "donggyu9410@gmail.com"
+    }
+  },
+  {
+    id: 327821,
+    title: "故 유채영 '이모션' 어쿠스틱커버 by 장범준 'Emotion' Acoustic COVER",
+    description:
+      "슈퍼스타K3 에서 잠깐 커버했던 이모션이란 노래입니다. 그간 팬분들이 계속 듣고 싶다하셨는데 이렇게 기회가 되어커버하게되었습니다~!오늘도 화이팅입니다~",
+    views: 263670,
+    videoFile: "https://www.youtube.com/embed/g-6cdv_fv3A",
+    creator: {
+      id: 376212,
+      name: "장범준",
+      email: "bumjun@naver.com"
+    }
+  },
+  {
+    id: 321709,
+    title: "The Chainsmokers - This Feeling ft. Kelsea Ballerini (Lyric Video)",
+    description:
+      "The Chainsmokers ft. Kelsea Ballerini 'This Feeling' out now: http://smarturl.it/TCSThisFeeling ",
+    views: 97956079,
+    videoFile: "https://www.youtube.com/embed/YuPzpoC3QNc?list=RDPh54wQG8ynk",
+    creator: {
+      id: 789213,
+      name: "b2_connected",
+      email: "AJ@naver.com"
+    }
+  }
+];
