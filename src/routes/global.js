7a1df65f4e14ac9db3ed582bdf44149ee8fbc193
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-16 20:20:41 +0900

    Adding controller to router

--- src/routes/global.js
+++ src/routes/global.js
@@ -6,11 +6,15 @@ import join from "../controllers/global/join";
 import logout from "../controllers/global/logout";
 import login from "../controllers/global/login";
 import search from "../controllers/global/search";
+import postJoin from "../controllers/global/postJoin";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
+
 globalRouter.get(routes.join, join);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.search, search);
