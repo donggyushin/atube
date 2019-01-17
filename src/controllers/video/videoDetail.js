83117e328d96f999bb461ebbd0b65c1a28cebe9a
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-17 22:30:24 +0900

    video detail page

--- src/controllers/video/videoDetail.js
+++ src/controllers/video/videoDetail.js
@@ -1,5 +1,17 @@
-const videoDetail = (req, res) => {
-  res.render("routes/VideoDetail/videoDetail.ejs");
+import Video from "../../models/Video";
+import routes from "../../routes/routes";
+
+const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    console.log(video);
+    res.render("routes/VideoDetail/videoDetail.ejs", { video });
+  } catch (err) {
+    res.redirect(routes.home);
+  }
 };
 
 export default videoDetail;
