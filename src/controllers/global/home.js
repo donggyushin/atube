28b1a3a300586fd9e263ad7dac5ddf85d26cda66
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-17 15:17:54 +0900

    Getting all videos from database

--- src/controllers/global/home.js
+++ src/controllers/global/home.js
@@ -1,12 +1,21 @@
-import { videos } from "../../db";
+import Video from "../../models/Video";
 
-const home = (req, res) => {
-  const local = {
-    pageTitle: "HOME",
-    videos
-  };
-
-  res.render("routes/Home/home.ejs", local);
+const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    const local = {
+      pageTitle: "HOME",
+      videos
+    };
+    res.render("routes/Home/home.ejs", local);
+  } catch (error) {
+    console.log(error);
+    const local = {
+      pageTitle: "HOME",
+      videos: []
+    };
+    res.render("routes/Home/home.ejs", local);
+  }
 };
 
 export default home;
