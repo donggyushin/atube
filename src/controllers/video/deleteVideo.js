4d02e52af5858fff7a5e1468360ba3cd35bd888d
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-18 16:46:46 +0900

    delete video

--- src/controllers/video/deleteVideo.js
+++ src/controllers/video/deleteVideo.js
@@ -1,5 +1,15 @@
-const deleteVideo = (req, res) => {
-  res.render("routes/DeleteVideo/deleteVideo.ejs");
+import routes from "../../routes/routes";
+import Video from "../../models/Video";
+
+const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {}
+  res.redirect(routes.home);
 };
 
 export default deleteVideo;
