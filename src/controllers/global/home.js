ab227bc4b63e468871c6ec7f717dfd8591c55104
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-20 00:28:03 +0900

    search page

--- src/controllers/global/home.js
+++ src/controllers/global/home.js
@@ -2,7 +2,7 @@ import Video from "../../models/Video";
 
 const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     const local = {
       pageTitle: "HOME",
       videos
--- src/controllers/global/search.js
+++ src/controllers/global/search.js
@@ -1,15 +1,22 @@
-import { videos } from "../../db";
-
-const search = (req, res) => {
+import Video from "../../models/Video";
+const search = async (req, res) => {
   const {
     query: { term: searchBy }
   } = req;
+  let videos = [];
   const local = {
     pageTitle: "SEARCH",
     searchBy,
     videos
   };
-  res.render("routes/Search/search.ejs", local);
+  try {
+    videos = await Video.find({ title: { $regex: searchBy, $options: "i" } });
+    local.videos = videos;
+    res.render("routes/Search/search.ejs", local);
+  } catch (error) {
+    console.log(error);
+    res.render("routes/Search/search.ejs", local);
+  }
 };
 
 export default search;
