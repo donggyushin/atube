605e68e93a65044e5d66662119027d73a44fbd69
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-20 04:26:45 +0900

    Webpack tutorial, and add sass and postcss, and i used extract-text-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const ExtractCss = require("extract-text-webpack-plugin");
+
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ENTRY_FILE,
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractCss.extract({
+          fallback: "style-loader", // css가 추출되지 않을 때 사용하는 로더
+          use: [
+            { loader: "css-loader" },
+            {
+              loader: "postcss-loader",
+              options: {
+                plugin() {
+                  return autoprefixer({ browsers: "cover 99.5%" });
+                }
+              }
+            },
+            { loader: "sass-loader" }
+          ],
+          publicPath: OUTPUT_DIR // 경로 재설정
+        })
+      }
+    ]
+  },
+  plugins: [new ExtractCss("styles.css")]
+};
+
+module.exports = config;
