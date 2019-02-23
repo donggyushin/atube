0d98fe639f8e943917abba29c2261174153f9192
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-02-24 00:03:26 +0900

    asd

--- src/controllers/user/facebookLogin.js
+++ src/controllers/user/facebookLogin.js
@@ -1,11 +1,33 @@
 import passport from "passport";
 import routes from "../../routes/routes";
+import User from "../../models/User";
 
-export const facebookLogin = (_, __, profile, cd) => {
-  console.log(profile);
+export const facebookLogin = async (_, __, profile, cb) => {
+  const { id, displayName, emails, photos } = profile;
+  try {
+    const user = await User.findOne({ facebookId: id });
+    if (user) {
+      user.facebookId = id;
+      user.avataUrl = photos[0].value;
+      user.save();
+      return cb(null, user);
+    }
+    const newUser = await User.create({
+      name: displayName,
+      email: emails[0].value,
+      avataUrl: photos[0].value,
+      facebookId: id
+    });
+
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error, null);
+  }
 };
 
-export const getFacebookLogin = passport.authenticate("facebook");
+export const getFacebookLogin = passport.authenticate("facebook", {
+  scope: ["email"]
+});
 
 export const getFacebookLoginCallback = passport.authenticate("facebook", {
   failureRedirect: routes.login
